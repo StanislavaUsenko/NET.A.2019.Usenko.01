6fb1cf2e3d85b261acfedb7a2b8cbec8895aaf21
StanislavaUsenko <52716541+StanislavaUsenko@users.noreply.github.com>
2019-08-08 14:31:51 +0300

    add merge sort

--- Program.cs
+++ Program.cs
@@ -10,14 +10,45 @@ namespace NET.A._2019.Usenko._01
     {
         static void Main(string[] args)
         {
+
             CheckQuickSort();
+            CheckMergeSort();
+        }
+
+        static void CheckMergeSort()
+        {
+            var arrayForCheck1 = new int[] { 9, 184, 1, 52, 461, 6, 265, 26, 7, 1, 5, 8, 435, 23, 6, 72, 34, 62, 345, 234, 62 };
+            var arrayForCheck2 = new int[] { -9, 184, 1, 52, -461, 6, 265, 26, -7, 1, 5, 8, 435, -23, 6, 72, 34, -62, 345, 234, 62 };
+            Console.WriteLine();
+            Console.WriteLine("Merge Sort:");
+            Console.WriteLine("Input 1: ");
+            for (int i = 0; i < arrayForCheck1.Length; i++)
+                Console.Write(arrayForCheck1[i] + " ");
+            Console.WriteLine("");
+            Console.WriteLine("Output 1: ");
+            arrayForCheck1 = MergeSortMethod.MergeSort(arrayForCheck1);
+            for (int i = 0; i < arrayForCheck1.Length; i++)
+                Console.Write(arrayForCheck1[i] + " ");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Input 2: ");
+            for (int i = 0; i < arrayForCheck2.Length; i++)
+                Console.Write(arrayForCheck2[i] + " ");
+            Console.WriteLine("");
+            Console.WriteLine("Output 2: ");
+            arrayForCheck2 = MergeSortMethod.MergeSort(arrayForCheck2);
+            for (int i = 0; i < arrayForCheck2.Length; i++)
+                Console.Write(arrayForCheck2[i] + " ");
+
+            Console.ReadKey();
         }
 
         static void CheckQuickSort()
         {
             var arrayForCheck1 = new int[] { 9, 184, 1, 52, 461, 6, 265, 26, 7, 1, 5, 8, 435, 23, 6, 72, 34, 62, 345, 234, 62 };
             var arrayForCheck2 = new int[] { -9, 184, 1, 52, -461, 6, 265, 26, -7, 1, 5, 8, 435, -23, 6, 72, 34, -62, 345, 234, 62 };
-
+            Console.WriteLine();
             Console.WriteLine("Quick Sort:");
             Console.WriteLine("Input 1: ");
             for (int i = 0; i < arrayForCheck1.Length; i++)
